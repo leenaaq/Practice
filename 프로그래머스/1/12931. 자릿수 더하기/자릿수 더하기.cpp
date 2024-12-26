519f328b283ce5cebae505b3d23a185094d16919
leena <100330547+leenaaq@users.noreply.github.com>
2024-12-26 18:32:34 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.02 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.cpp
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.cpp
@@ -5,30 +5,10 @@ int solution(int n)
 {
     int answer = 0;
     
-    answer += n / 100000000;
-    n %= 100000000;
-    
-    answer += n / 10000000;
-    n %= 10000000;
-    
-    answer += n / 1000000;
-    n %= 1000000;
-    
-    answer += n / 100000;
-    n %= 100000;
-    
-    answer += n / 10000;
-    n %= 10000;
-    
-    answer += n / 1000;
-    n %= 1000;
-    
-    answer += n / 100;
-    n %= 100;
-    
-    
-    answer += n / 10;
-    n %= 10;
+    while(n){
+        answer += n % 10;
+        n /= 10;
+    }
 
     answer += n;
     
