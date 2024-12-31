84e581922cb0f214af126de6e329d5921c530881
leena <100330547+leenaaq@users.noreply.github.com>
2024-12-31 13:06:49 +0900

    [Bronze II] Title: 별 찍기 - 21, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10996. 별 찍기 － 21/별 찍기 － 21.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n; cin >> n;
+
+	if (n == 1) cout << "*\n";
+	else
+	{
+		for (int i = 0; i < n; ++i)
+		{
+			for (int j = 0; j < n / 2 + n % 2; ++j) cout << "* ";
+			cout << "\n";
+			for (int j = 0; j < n / 2; ++j) cout << " *";
+			cout << "\n";
+		}
+	}
+    return 0;
+}
