362dec66194d0dda7364dd63589a2da5828369d6
leena <100330547+leenaaq@users.noreply.github.com>
2025-01-01 22:39:17 +0900

    [level 1] Title: 과일 장수, Time: 24.21 ms, Memory: 34.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(int k, int m, vector<int> score) {
+    int answer = 0;
+    sort(score.begin(), score.end(), greater<int>());
+
+    for (int i = 0; i < score.size() / m; i++) {
+        int temp = score[(i + 1) * m - 1];
+        answer += temp * m;
+    }
+
+    return answer;
+}
