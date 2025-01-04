d491810b0416d22eed038f58dc0be1bfeb115013
leena <100330547+leenaaq@users.noreply.github.com>
2025-01-04 19:06:28 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 0.29 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.cpp
@@ -0,0 +1,44 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+string ejin(int n) {
+    vector<int> temp;
+    while(n > 1){
+        temp.push_back(n % 2);
+        n /= 2;
+    }
+    if(n == 1) temp.push_back(n);
+    
+    string temp2;
+    while(!temp.empty()){
+        temp2 += to_string(temp.back());  
+        temp.pop_back();
+    }
+    //cout << "\n" << temp2 << "\n";
+    return temp2; 
+}
+
+int Count(string s, int *zeroCnt){
+    int oneCnt = 0;
+    for(int i = 0; i < s.length(); i++){
+        if(s[i] == '0') (*zeroCnt)++;
+        else if(s[i] == '1') oneCnt++;
+    }
+    return oneCnt;
+}
+
+vector<int> solution(string s) {
+    vector<int> answer;
+    int cnt = 0, len, zeroCnt = 0;
+    while(len != 1){
+        len = Count(s, &zeroCnt);
+        s = ejin(len);
+        cnt++;
+    }
+    answer.push_back(cnt);
+    answer.push_back(zeroCnt);
+    return answer;
+}
