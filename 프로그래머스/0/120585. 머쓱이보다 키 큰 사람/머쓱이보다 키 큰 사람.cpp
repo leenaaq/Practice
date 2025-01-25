b042bc36340044bf220db0ddce70da52208f4b7b
leena <100330547+leenaaq@users.noreply.github.com>
2025-01-25 23:22:17 +0900

    Update 머쓱이보다 키 큰 사람.cpp

--- 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.cpp
+++ 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.cpp
@@ -1,13 +1,10 @@
 #include <string>
 #include <vector>
-#include <algorithm>
-#include <iostream>
 
 using namespace std;
 
 int solution(vector<int> array, int height) {
     int answer = 0;
-    sort(array.begin(), array.end(), greater<int>());
     
     for(int arr : array){
         if(arr > height) answer++;
