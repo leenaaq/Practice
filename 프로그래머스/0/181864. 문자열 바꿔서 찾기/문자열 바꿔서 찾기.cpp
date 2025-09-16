8b1f36f958a71cd63786c5cc40a86e32d1e63de6
leenaaq <100330547+leenaaq@users.noreply.github.com>
2025-09-17 04:58:29 +0900

    [level 0] Title: 문자열 바꿔서 찾기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.cpp
+++ 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.cpp
@@ -1,29 +1,16 @@
 #include <string>
 #include <vector>
 #include <map>
-#include <iostream>
+#include <string>
 
 using namespace std;
 
 int solution(string myString, string pat) {
     map <char, char>m {{'A', 'B'}, {'B', 'A'}};
-    int answer;
-    
     for(int i = 0; i < myString.length(); i++)
     {
-        answer = 0;
-        for(int j = 0; j < pat.length(); j++)
-        {
-            if(m[myString[i + j]] == pat[j])
-            {
-                answer++;
-            }
-            else
-            {
-                break;
-            }
-        }
-        if(answer == pat.length()) return answer = 1;       
+        myString[i] = m[myString[i]];
     }
-    return answer = 0;
+    if(myString.find(pat) != string::npos) return 1;
+    else return 0;
 }
