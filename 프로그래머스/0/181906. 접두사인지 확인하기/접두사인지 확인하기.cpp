4a3265553617d7d47661e5ad14b6e72305fdc767
leena <100330547+leenaaq@users.noreply.github.com>
2025-09-17 02:20:40 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 0.01 ms, Memory: 3.67 MB -BaekjoonHub

---
+++ 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string my_string, string is_prefix) {
+    int answer = 0;
+    for(int i = 0; i < is_prefix.length(); i++)
+    {
+        if(my_string[i] != is_prefix[i])
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
