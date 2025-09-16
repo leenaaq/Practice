920a59269f0e556b0f1af2e1a8b404eec4981304
leenaaq <100330547+leenaaq@users.noreply.github.com>
2025-09-17 06:12:56 +0900

    [level 0] Title: 특별한 이차원 배열 1, Time: 2.17 ms, Memory: 5.81 MB -BaekjoonHub

--- 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.cpp
+++ 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.cpp
@@ -4,14 +4,10 @@
 using namespace std;
 
 vector<vector<int>> solution(int n) {
-    vector<vector<int>> answer(n, vector<int>(n));
+    vector<vector<int>> answer(n, vector<int>(n, 0));
     for(int i = 0; i < n; i++)
     {
-        for(int j = 0; j < n; j++)
-        {
-            if(i == j) answer[i][j] = 1;
-            else answer[i][j] = 0;
-        }
+        answer[i][i] = 1;
     }
     return answer;
 }
