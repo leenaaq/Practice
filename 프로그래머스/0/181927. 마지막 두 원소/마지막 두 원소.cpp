0b336759d99858a961f5bb739f5de41ec1a1c205
leena <100330547+leenaaq@users.noreply.github.com>
2025-09-17 03:16:15 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.02 ms, Memory: 4.12 MB -BaekjoonHub

---
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> num_list) {
+    int n = num_list.size() - 1;
+    if(num_list[n] > num_list[n - 1])
+    {
+        num_list.push_back(num_list[n] - num_list[n-1]);
+    }
+    else
+    {
+        num_list.push_back(num_list[n] * 2);
+    }
+    return num_list;
+}
