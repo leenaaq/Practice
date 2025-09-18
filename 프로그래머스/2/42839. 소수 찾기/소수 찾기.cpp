98562b0866123b4261456f4cdf767bcfaea636d5
leenaaq <100330547+leenaaq@users.noreply.github.com>
2025-09-19 03:54:14 +0900

    [level 2] Title: 소수 찾기, Time: 0.67 ms, Memory: 4.13 MB -BaekjoonHub

---
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.cpp
@@ -0,0 +1,59 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <set>
+using namespace std;
+
+bool isPrime(int n)
+{
+    if(n < 2) 
+    {
+        return false;
+    }
+    
+    for(int i = 2; i * i <= n; i++)
+    {
+        if(n % i == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+int solution(string numbers) {
+    vector<char> num;
+    set<int> answer;
+    int cnt = 0;
+    
+    for(int i = 0; i < numbers.length(); i++)
+    {
+        num.push_back(numbers[i]);
+    }
+
+    sort(num.begin(), num.end());
+
+    do
+    {
+        for(int k = 1; k <= num.size(); k++)
+        {
+            string tmp = "";
+            for(int j = 0; j < k; j++)
+            {
+                tmp += num[j];
+            }
+            answer.insert(stoi(tmp));
+        }
+    }
+    while(next_permutation(num.begin(), num.end()));
+
+    for(auto x : answer)
+    {
+        if(isPrime(x))
+        {
+            cnt++;
+        }
+    }
+    return cnt;
+}
