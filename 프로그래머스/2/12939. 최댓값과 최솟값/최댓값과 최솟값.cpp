9409dc22a942a0ab6870faf3505998ad14b5a0d8
leenaaq <100330547+leenaaq@users.noreply.github.com>
2025-09-18 23:18:42 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.04 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.cpp
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.cpp
@@ -1,11 +1,12 @@
 #include <string>
 #include <vector>
+#include <algorithm>
+#include <iostream>
 
 using namespace std;
 
 string solution(string s) {
     string str = "";
-    int min, max;
     vector<int> v;
     
     for(int i = 0; i < s.length(); i++)
@@ -20,22 +21,8 @@ string solution(string s) {
             str += s[i];
         }
     }
-    
     v.push_back(stoi(str));
-    min = v[0];
-    max = v[0];
-    
-    for(int i = 1; i < v.size(); i++)
-    {
-        if(max < v[i])
-        {
-            max = v[i];
-        }
-        if(min > v[i])
-        {
-            min = v[i];
-        }
-    }
-    
-    return to_string(min) + " " + to_string(max);
+    sort(v.begin(), v.end());
+        
+    return to_string(v[0]) + " " + to_string(v[v.size() - 1]);
 }
