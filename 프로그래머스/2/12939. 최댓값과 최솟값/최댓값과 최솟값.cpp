2fa29a58fe313a6357a43301055d00d4acc16d30
leenaaq <100330547+leenaaq@users.noreply.github.com>
2025-09-18 23:06:43 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.03 ms, Memory: 3.63 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.cpp
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.cpp
@@ -1,40 +1,41 @@
-#include <iostream>
-#include <vector>
 #include <string>
-#include <algorithm>
+#include <vector>
+
 using namespace std;
 
 string solution(string s) {
-    string answer = "", str = "";
+    string str = "";
+    int min, max;
     vector<int> v;
-    for (int i = 0; i < s.length(); i++) {
-        if (s[i] == ' ') {
+    
+    for(int i = 0; i < s.length(); i++)
+    {
+        if(s[i] == ' ')
+        {
             v.push_back(stoi(str));
             str = "";
         }
-        else str += s[i];
+        else
+        {
+            str += s[i];
+        }
     }
+    
     v.push_back(stoi(str));
-
-    int min = v[0], max = v[0];
-    for (int i = 0; i < v.size(); i++) {
-        if (v[i] < min) min = v[i];
-        if (v[i] > max) max = v[i];
+    min = v[0];
+    max = v[0];
+    
+    for(int i = 1; i < v.size(); i++)
+    {
+        if(max < v[i])
+        {
+            max = v[i];
+        }
+        if(min > v[i])
+        {
+            min = v[i];
+        }
     }
-    answer += to_string(min);
-    answer += " ";
-    answer += to_string(max);
-
-    return answer;
-}
-
-int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-
-    string s;
-    getline(cin, s);
-    cout << solution(s);
-
-    return 0;
+    
+    return to_string(min) + " " + to_string(max);
 }
